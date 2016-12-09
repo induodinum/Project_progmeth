c4c85cebc96cc88e230c403e19dd11e0646469a1
natttttt <natttttt1@hotmail.com>
2016-12-10 00:07:30 +0700

    hahaa

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root, 800, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			System.out.println("hello");
 			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
