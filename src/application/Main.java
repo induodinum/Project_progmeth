83c96f12f1389d7e89b923167c1f3b6ea35ac765
Indy <Indy@DESKTOP-48D8FC0>
2016-12-10 00:00:59 +0700

    yae hae hae

---
+++ src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			GridPane root = new GridPane();
+			Image brick = new Image(ClassLoader.getSystemResource("brick.jpg").toString(), 200, 100, false, false);
+			Scene scene = new Scene(root, 800, 600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
